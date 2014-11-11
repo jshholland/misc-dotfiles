9291dc62075b5e4e9122c717f60053f389ca64ee
Josh Holland <josh@inv.alid.pw>
2014-11-11 13:45:05 +0000

    Switch from greedyView to view
    This makes the multi-monitor support rather nicer.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,13 +1,15 @@
-import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Actions.UpdatePointer
-import XMonad.Util.Run          (spawnPipe)
-import System.IO
+import           XMonad
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Actions.UpdatePointer
+import           XMonad.Util.Run              (spawnPipe)
+import           XMonad.Util.EZConfig
+import qualified XMonad.StackSet              as W
+import           System.IO
 
 main = do
     xmobar <- spawnPipe "xmobar"
-    xmonad defaultConfig
+    xmonad $ defaultConfig
         { modMask     = mod4Mask
         , terminal    = "urxvt"
         , borderWidth = 3
@@ -19,4 +21,16 @@ main = do
                 , ppTitle  = xmobarColor "green" "" . shorten 100
                 }
             updatePointer $ Relative 0.5 0.5
-        }
+        } `additionalKeysP`
+        -- switch mod-<number> from greedyView to view so that if a workspace
+        -- is currently displayed on another monitor, focus shifts instead of the
+        -- workspaces swapping
+        [ (extraModMasks ++ "M-" ++ ws, action ws)
+          | ws <- take 9 $ map show [1..]
+          , (extraModMasks, action) <- [ ("",   windows . W.view)
+                                       , ("S-", windows . W.shift)
+                                       ]
+        ]
+          
+
+

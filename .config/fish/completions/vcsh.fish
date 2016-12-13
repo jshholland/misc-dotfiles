4577e5b8efd51214ff13eef60192d3470c623133
Josh Holland <josh@inv.alid.pw>
2016-12-13 01:45:38 +0000

    stuff

--- .config/fish/completions/vcsh.fish
+++
@@ -1,8 +0,0 @@
-complete -c vcsh -a 'commit help list list-tracked pull push version clone which init'
-complete -c vcsh -a 'delete enter list-tracked-by rename run upgrade write-gitignore'
-complete -c vcsh -s v -d 'Enable verbose mode'
-complete -c vcsh -s d -d 'Enable debug mode'
-
-for repo in (vcsh list)
-	complete -c vcsh -a $repo
-end
